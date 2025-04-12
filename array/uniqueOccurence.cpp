e01072de180e1eb05d0107eb1ce1c2d9f61b5a02
AdityaRaj019 <adityaraj9341376019@gmail.com>
2025-04-13 00:46:45 +0530

    Initial commit

---
+++ array/uniqueOccurence.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+bool uniqueOccurence(int arr[],int n){
+    int freq = 0;
+    
+}
+
+
+
+int main() {
+      
+    cout << "Hello, World!" << endl;
+    return 0;
+}
--- class/4pillar.cpp
+++ class/4pillar.cpp
@@ -19,26 +19,62 @@ class human{
      void getData(){
         cout<<"[Height:"<<height<<", Weight:"<<weight<<", age:" << age<<"]" <<endl;
      }
+     void operator+(human &h1){
+      human h;
+      int value1  = this->height;
+      int value2 = h1.weight;
+      cout<< value1 - value2;
+     }
 };
 class male : public human{
     public:
     male(){height=0;}
 
     string color;
+    male(int height,int weight,int age,const string &color){
+      this->height=height;
+      this->weight=weight;
+      this->age=age;
+      this->color = color;
+
+   }
     void rand(){
-        cout<<"male is rand0m"<<endl;
+        cout<<"male is balck"<<endl;
+    }
+    void getData(){
+      cout<<"[Height:"<<height<<", Weight:"<<weight<<", age:" << age<<", Color:"<<color<<"]" <<endl;
     }
 };
 
 int main() {
-      
-    male m1;
-    m1.age = 78;
-    m1.color = "black";
-    // m1.height = 55;
-    m1.weight = 45;
-    m1.getData();
+   
+   
+   // Inheritence
 
-    cout << "Hello, World!" << endl;
+   //  male m1;
+   //  m1.age = 78;
+   //  m1.color = "black";
+   //  // m1.height = 55;
+   //  m1.weight = 45;
+   //  m1.getData();
+
+   //  cout << "Hello, World!" << endl;
+
+   // Polymorphism
+
+   // human h3,h4;
+   // h3.height = 4;
+   // h4.weight = 5;
+
+   // h3+h4;
+
+   human h1(6,78,8);
+   male m1(4,5,6,"kalla");
+   
+   h1.getData();
+   m1.getData();
+
+ 
+
     return 0;
 }
