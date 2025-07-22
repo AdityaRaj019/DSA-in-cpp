c8f1233b3b9e6b1e7db99ddfa5bad02f3787abfe
AdityaRaj019 <adityaraj9341376019@gmail.com>
2025-07-22 21:48:18 +0530

    Solve: optimal

--- question/string/validPalindrome.cpp
+++ question/string/validPalindrome.cpp
@@ -16,33 +16,45 @@ char toLower(char ch)
         return temp;
     }
 }
-// void isAlnum(char c){
-//     if(c==" "||c=="\t"||c=="\n"||c=="\r"||c=="@"||c=="#"||c==
-// }
+bool isAlnum(char c){
+    if((c>='a' && c<='z') || (c>='A' && c<='Z' )|| (c>='0' && c<='9' )) return 1;
+    return 0;
+}
 bool palin(string s)
 {
     int n = s.size();
-    int i = 0;
-    int j = n - 1;
-    while (i < j)
-    {
-        while ((i < j) && ((s[i] >= 32 && s[i] <= 47) || (s[i] >= 58 && s[i] <= 64) || (s[i] >= 91 && s[i] <= 96) || (s[i] >= 123 && s[i] <= 126)))
-        {
-            i++;
+    string temp = "";
+    // int i = 0;
+    // int j = n - 1;
+    // while (i < j)
+    // {
+    //     while ((i < j) && ((s[i] >= 32 && s[i] <= 47) || (s[i] >= 58 && s[i] <= 64) || (s[i] >= 91 && s[i] <= 96) || (s[i] >= 123 && s[i] <= 126)))
+    //     {
+    //         i++;
+    //     }
+    //     while (i < j && ((s[j] >= 32 && s[j] <= 47) || (s[j] >= 58 && s[j] <= 64) || (s[j] >= 91 && s[j] <= 96) || (s[j] >= 123 && s[j] <= 126)))
+    //     {
+    //         j--;
+    //     }
+    //     if (i < j && (toLower(s[i]) != toLower(s[j])))
+    //     {
+    //         return false;
+    //     }
+    //     i++;
+    //     j--;
+    //     // else if(toLower(s[i])!=toLower(s[j])
+    // }
+    for(int i  = 0 ; i < n ;i++){
+        if(isAlnum(s[i])){
+            temp.push_back(toLower(s[i]));
         }
-        while (i < j && ((s[j] >= 32 && s[j] <= 47) || (s[j] >= 58 && s[j] <= 64) || (s[j] >= 91 && s[j] <= 96) || (s[j] >= 123 && s[j] <= 126)))
-        {
-            j--;
-        }
-        if (i < j && (toLower(s[i]) != toLower(s[j])))
-        {
-            return false;
-        }
-        i++;
-        j--;
-        // else if(toLower(s[i])!=toLower(s[j])
     }
-    return true;
+    string rev = temp;
+    reverse(rev.begin(),rev.end());
+    if(temp==rev){
+        return true;
+    }
+    return false;
 }
 
 using namespace std;
@@ -50,6 +62,7 @@ using namespace std;
 int main()
 {
     cout<< palin("A man, a plan, a canal: Panama");
+
 
     // cout << "Hello, World!" << endl;
     return 0;
