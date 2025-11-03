66acd8977d1d75058fa0cb1fdd4087754c7820b9
AdityaRaj019 <adityaraj9341376019@gmail.com>
2025-11-03 23:40:11 +0530

    Implement:Brute force

---
+++ recursion/inversion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Brute force
+// int inversion(vector<int>&arr){
+//     int n = arr.size();
+//     int cnt= 0;
+//     for(int i=0;i<n-1;i++){
+//         for(int j=i+1;j<n;j++){
+//             if(arr[i]>arr[j]){
+//                 cnt++;
+//             }
+//         }
+//     }
+//     return cnt;
+// }
+//Optimal merge sort approach
+
+
+
+
+
+
+int main() {
+      vector<int>arr = {2,4,1,3,5};
+    cout << inversion(arr) << endl;
+    cout << "" << endl;
+    return 0;
+}
